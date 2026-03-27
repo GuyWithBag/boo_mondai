d9c0fa7c73ee9598cf370d17002e1e5b53b7264d
LoejeeMan <ldulaugon123@gmail.com>
2026-03-27 17:51:23 +0800

    refactor: use barrel files instead

--- lib/pages/review_page.dart
+++ lib/pages/review_page.dart
@@ -10,13 +10,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
-import 'package:boo_mondai/providers/auth_provider.dart';
-import 'package:boo_mondai/providers/fsrs_provider.dart';
-import 'package:boo_mondai/providers/streak_provider.dart';
-import 'package:boo_mondai/shared/app_spacing.dart';
-import 'package:boo_mondai/shared/theme_constants.dart';
-import 'package:boo_mondai/widgets/quiz_question_card.dart';
-import 'package:boo_mondai/widgets/self_rating_bottom_sheet.dart';
+import 'package:boo_mondai/providers/providers.dart';
+import 'package:boo_mondai/shared/shared.dart';
+import 'package:boo_mondai/widgets/widgets.dart';
 
 class ReviewPage extends HookWidget {
   const ReviewPage({super.key});
