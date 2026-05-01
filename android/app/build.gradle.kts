d808498831d077ac58b094ae091965306254f20b
LoejeeMan <ldulaugon123@gmail.com>
2026-05-01 22:21:47 +0800

    feat: Added flutter_local_notifications support for android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -37,6 +38,15 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    val billing_version = "8.3.0"
+
+    implementation("com.android.billingclient:billing-ktx:$billing_version")
+    implementation("androidx.window:window:1.0.0")
+    implementation("androidx.window:window-java:1.0.0")
 }
 
 flutter {
